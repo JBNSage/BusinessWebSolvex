4ec8832498e04cb84512a454e8788a59642bd342
Juan Luis Bello <jlbello24@gmail.com>
2022-05-05 00:15:26 -0400

    styled place order

--- bwfront/src/views/checkout/PlaceOrder.js
+++ bwfront/src/views/checkout/PlaceOrder.js
@@ -25,10 +25,19 @@ export default function PlaceOrder() {
   }
 
   return (
-    <div>
-      {placingOrder !== undefined && createdOrder.id}
+    <div className="place_order_container">
+      {placingOrder !== undefined && (
+        <div class="alert alert-success" role="alert">
+          <h4 class="alert-heading">Order placed!</h4>
+          <p>Your order has been successfully placed</p>
+          <hr />
+          <p class="mb-0">Order id: {createdOrder.id}</p>
+        </div>
+      )}
 
-      <Link to="../../profile/my-orders">Mis órdenes</Link>
+      <Link to="../../profile/my-orders" className="btn btn-dark">
+        My orders
+      </Link>
     </div>
   );
 }
